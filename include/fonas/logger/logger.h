d81666b2aaea1f5814313c7bd11f76ee5c960d05
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-05-17 18:25:02 +0300

    feat: fonas/logger C macros

--- include/fonas/logger/logger.h
+++ include/fonas/logger/logger.h
@@ -26,6 +26,14 @@ extern "C"
 
     void fonas_logger_log(LoggerModule *module, LoggerLevel level, const char *fmt, ...);
 
+#define LOG_SCOPE(scope) LoggerModule logger_module = {.name = #scope, .log_level = LOGGER_LEVEL_NOTSET};
+#define LOG(level, ...) fonas_logger_log(&logger_module, level, __VA_ARGS__);
+#define LOG_DEBUG(...) LOG(LOGGER_LEVEL_DEBUG, __VA_ARGS__)
+#define LOG_INFO(...) LOG(LOGGER_LEVEL_INFO, __VA_ARGS__)
+#define LOG_WARNING(...) LOG(LOGGER_LEVEL_WARNING, __VA_ARGS__)
+#define LOG_ERROR(...) LOG(LOGGER_LEVEL_ERROR, __VA_ARGS__)
+#define LOG_CRITICAL(...) LOG(LOGGER_LEVEL_CRITICAL, __VA_ARGS__)
+
 #ifdef __cplusplus
 }
 #endif
