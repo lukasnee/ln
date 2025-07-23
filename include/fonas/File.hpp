5e29261360e6e6b4184da017195af1db1d68197d
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-07-23 23:36:48 +0300

    chore: add .clang-format and .cmake-format and format everything

--- include/fonas/File.hpp
+++ include/fonas/File.hpp
@@ -9,7 +9,7 @@
 
 #pragma once
 
-#include <cstdio> 
+#include <cstdio>
 
 namespace fonas {
 
--- include/fonas/StreamBuffer.hpp
+++ include/fonas/StreamBuffer.hpp
@@ -21,10 +21,10 @@
 namespace fonas {
 
 /**
- * @brief Stream Buffer. Stream buffers allow a stream of bytes to be passed from an interrupt service routine to a task, or
- * from one task to another task. A byte stream can be of arbitrary length and does not necessarily have a beginning or end. Any
- * number of bytes can be written in one go, and any number of bytes can be read in one go. Data is passed by copy - the data is
- * copied into the buffer by the sender and out of the buffer by the read.
+ * @brief Stream Buffer. Stream buffers allow a stream of bytes to be passed from an interrupt service routine to a
+ * task, or from one task to another task. A byte stream can be of arbitrary length and does not necessarily have a
+ * beginning or end. Any number of bytes can be written in one go, and any number of bytes can be read in one go. Data
+ * is passed by copy - the data is copied into the buffer by the sender and out of the buffer by the read.
  *
  * @ref https://www.freertos.org/RTOS-stream-buffer-example.html
  */
@@ -34,8 +34,8 @@ public:
      * @brief Construct a new Stream Buffer.
      *
      * @param buffer_size
-     * @param trigger_level The number of bytes that must be in the stream buffer before a task that is blocked on the stream
-     * buffer to wait for data is moved out of the blocked state.
+     * @param trigger_level The number of bytes that must be in the stream buffer before a task that is blocked on the
+     * stream buffer to wait for data is moved out of the blocked state.
      */
     StreamBuffer(std::size_t buffer_size, std::size_t trigger_level);
 
@@ -72,8 +72,8 @@ public:
      * @param data
      * @param size
      * @param timeout_ticks
-     * @return std::size_t The number of bytes actually read. If it is less than the size requested, it is known that the read
-     * timed out.
+     * @return std::size_t The number of bytes actually read. If it is less than the size requested, it is known that
+     * the read timed out.
      */
     std::size_t receive(std::uint8_t *data, std::size_t size, TickType_t timeout_ticks = portMAX_DELAY);
 
@@ -85,7 +85,8 @@ public:
      * @param higher_priority_task_woken
      * @return std::size_t
      */
-    std::size_t receive_from_isr(std::uint8_t *data, std::size_t size, BaseType_t *const higher_priority_task_woken = nullptr);
+    std::size_t receive_from_isr(std::uint8_t *data, std::size_t size,
+                                 BaseType_t *const higher_priority_task_woken = nullptr);
 
     /**
      * @brief Check if the buffer is full.
@@ -126,8 +127,8 @@ public:
     std::size_t bytes_available() const;
 
     /**
-     * @brief Set the trigger level - the number of bytes that must be in the stream buffer before a task that is blocked on the
-     * stream buffer to wait for data is moved out of the blocked state.
+     * @brief Set the trigger level - the number of bytes that must be in the stream buffer before a task that is
+     * blocked on the stream buffer to wait for data is moved out of the blocked state.
      *
      * @param trigger_level
      *
