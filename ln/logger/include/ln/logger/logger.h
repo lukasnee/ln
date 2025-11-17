4d52b4be5bb1b2db643cad9548425933f0eeb131
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-11-18 00:22:25 +0200

    chore: add preambles to source files using preamble.fish

--- ln/logger/include/ln/logger/logger.h
+++ ln/logger/include/ln/logger/logger.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2025 Lukas Neverauskis https://github.com/lukasnee
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 #pragma once
 
 #ifdef __cplusplus
--- ln/shell/include/ln/shell/generic/cmds.hpp
+++ ln/shell/include/ln/shell/generic/cmds.hpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025 Lukas Neverauskis https://github.com/lukasnee
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
 #include "ln/shell/CLI.hpp"
 
 namespace ln::shell::generic::cmds {
