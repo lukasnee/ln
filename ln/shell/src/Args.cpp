3e2df0d4c0d014ab7c59443e815b5c5a1af46474
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-12-07 18:29:45 +0200

    feat(shell): single and double quoted input parsing

--- ln/shell/src/Args.cpp
+++ ln/shell/src/Args.cpp
@@ -10,8 +10,8 @@
 #include "ln/shell/Args.hpp"
 
 #include <string_view>
-#include <algorithm> 
-#include <cstdio> 
+#include <algorithm>
+#include <cstdio>
 #include <optional>
 #include <ranges>
 
@@ -21,24 +21,52 @@ std::optional<std::span<std::string_view>> Args::tokenize(const std::string_view
                                                           std::span<std::string_view> args_buf) {
     size_t arg_count = 0;
     const char *arg_begin = nullptr;
+    char quote_char = '\0';
     for (const char *head = sv.data(); static_cast<size_t>(head - sv.data()) < sv.length() && *head != '\0'; head++) {
-        if (*head == ' ') {
-            if (!arg_begin) {
-                continue;
+        if (quote_char != '\0') {
+            if (*head == quote_char) {
+                quote_char = '\0';
+                if (arg_begin) {
+                    if (arg_count >= args_buf.size()) {
+                        return std::nullopt; // not enough space in args_buf
+                    }
+                    args_buf[arg_count++] = std::string_view(arg_begin, head - arg_begin);
+                    arg_begin = nullptr;
+                }
             }
-            if (arg_count >= args_buf.size()) {
-                return std::nullopt;
-            }
-            args_buf[arg_count++] = std::string_view(arg_begin, head - arg_begin);
-            arg_begin = nullptr;
         }
-        else if (!arg_begin) {
-            arg_begin = head;
+        else {
+            if (*head == '\'' || *head == '"') {
+                if (arg_begin) {
+                    if (arg_count >= args_buf.size()) {
+                        return std::nullopt; // not enough space in args_buf
+                    }
+                    args_buf[arg_count++] = std::string_view(arg_begin, head - arg_begin);
+                }
+                quote_char = *head;
+                arg_begin = head + 1;
+            }
+            else if (*head == ' ') {
+                if (!arg_begin) {
+                    continue;
+                }
+                if (arg_count >= args_buf.size()) {
+                    return std::nullopt; // not enough space in args_buf
+                }
+                args_buf[arg_count++] = std::string_view(arg_begin, head - arg_begin);
+                arg_begin = nullptr;
+            }
+            else if (!arg_begin) {
+                arg_begin = head;
+            }
         }
     }
     if (arg_begin) {
         if (arg_count >= args_buf.size()) {
             return std::nullopt;
+        }
+        if (quote_char != '\0') {
+            return std::nullopt; // unmatched quote
         }
         args_buf[arg_count++] = std::string_view(arg_begin, sv.data() + sv.length() - arg_begin);
     }
